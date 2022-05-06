15d659cc610608c5ea621f6687912c53a34c6c30
luis-wsky <luis.wsky@gmail.com>
2022-05-05 22:37:16 -0300

    manipulacao.dom

---
+++ index.js
@@ -0,0 +1,14 @@
+function acendaLuz(){
+    let image = window.document.getElementById("lampada");
+   image.src = "imagens/lampada_acessa.jpg"; 
+}
+function apagaLuz(){
+    let image = window.document.getElementById("lampada");
+    image.src = "imagens/lampada_apagada.jpg";
+}
+function quebraLampada(){
+    let image = window.document.getElementById("lampada");
+    image.src = "imagens/lampada_quebrada.jpg";
+}
+
+
